80c6b78a00c5bdfd5f5e03d8da857251cf7940c2
PranayChirlanchi70 <chirlanchip@gmail.com>
2023-06-13 11:58:44 +0530

    first commit

---
+++ hitesh/types.js
@@ -0,0 +1,18 @@
+let name = "pranay";
+let age = 18;
+let isLoggedIn = false;
+
+
+// prmitive  /////////////////////////
+
+// number => Range 2 to power 53
+// bigint
+// string => ""
+// boolean => true/ false
+// null => standalone value
+// undefined => not defined or not usaigned
+// symbol => it use most of in React for findin unique 
+
+// non-prmitive /////////////
+
+// object    // null is type of object
